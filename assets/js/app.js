f200adca786714b9cc9dbb1473e8f392907a8e88
midix <76846706+midixx@users.noreply.github.com>
2022-11-29 20:58:38 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+$(function() {
+
+    var header = $("#header"), 
+        introH = $("#intro").innerHeight(),
+        scrollOffset = 0;
+    
+    $(window).on("scroll", function() {
+
+        scrollOffset = $(this).scrollTop();
+
+        if( scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+    });
+ 
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $this.addClass('active');
+
+        $('html, body').animate({
+            scrollTop: blockOffset
+        }, 1000);
+    });
+
+    $('[data-collapse]').on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+        
+        $this.toggleClass("active");
+    });
+
+
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+
+});
